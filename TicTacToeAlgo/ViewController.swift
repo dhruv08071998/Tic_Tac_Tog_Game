874d39f8272f7af45be2b1b70b5c45a218585826
dhruv <dhruv@quarkstudios.com>
2020-04-04 11:03:31 +0530

    Initial Commit

---
+++ TicTacToeAlgo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TicTacToeAlgo
+//
+//  Created by Dhruv Upadhyay on 04/04/20.
+//  Copyright © 2020 Dhruv Upadhyay. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ TicTacToeAlgoTests/TicTacToeAlgoTests.swift
@@ -0,0 +1,34 @@
+//
+//  TicTacToeAlgoTests.swift
+//  TicTacToeAlgoTests
+//
+//  Created by Dhruv Upadhyay on 04/04/20.
+//  Copyright © 2020 Dhruv Upadhyay. All rights reserved.
+//
+
+import XCTest
+@testable import TicTacToeAlgo
+
+class TicTacToeAlgoTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
